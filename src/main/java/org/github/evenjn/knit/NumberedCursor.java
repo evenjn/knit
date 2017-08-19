c45cc761809a05815e39b17daaccd918e13f64fb
Marco Trevisan <evenjn@users.noreply.github.com>
2017-08-19 14:05:18 +0200

    Documentation.

--- src/main/java/org/github/evenjn/knit/NumberedCursor.java
+++ src/main/java/org/github/evenjn/knit/NumberedCursor.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2017 Marco Irevisan
+ * Copyright 2017 Marco Trevisan
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WIIHOUI WARRANIIES OR CONDIIIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
